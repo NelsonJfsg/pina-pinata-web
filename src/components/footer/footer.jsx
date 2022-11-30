6da9bce45415277b5d955d1a74b2aa5af86e8250
Tacho1234 <grijalvatacho@gmail.com>
2022-11-30 15:12:59 -0700

    Footer Seccion

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,17 @@
+import './footerStyle.css';
+
+export const Footer = () => {
+
+    return(
+
+        <>
+        
+        <footer class="footer-seccion">
+            <p class='copyright'>Todos los derechos reservados 2022 &copy;</p>
+        </footer>
+
+        </>
+
+    );
+
+}
--- src/pages/auth/login/login.jsx
+++ src/pages/auth/login/login.jsx
@@ -1,5 +1,6 @@
 import "./loginStyle.css";
 import { CredentialForm } from "../../../components/credentialForm/credential-login-form/credentialForm";
+import { Footer } from "../../../components/footer/footer";
 
 const validation = () => {
     alert('Crdenciales incorrectas');
@@ -14,7 +15,9 @@ export const Login = () => {
             <div className="container">
                 <CredentialForm buttonText = "Login"/>
             </div>
+            <div>
+                <Footer/>
+            </div>
         </>
-
     );
 }
